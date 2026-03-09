1ceb64f81f446b7b06e073c87b732e867cd8e691
TristanPilatPXL <Tristan.Pilat@STUDENT.PXL.BE>
2026-03-09 11:15:49 +0100

    io-02: methodes voor file in te lezen met stringbuilder

--- FileI-O/MainWindow.xaml.cs
+++ FileI-O/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,54 @@ namespace FileI_O
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+
+        //text ergen in krijgen
+        private void readFileButon_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder result = ReadEmailFile(@"C:\Users\Trist\Desktop\Graduaat\C-Advanced\FileI-O\FileI-O\Temp-folder\TextFile1.txt");
+
+        }
+
+        //streamreader
+        StringBuilder ReadEmailFile(string filepath)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            using (StreamReader reader = new StreamReader(filepath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    sb.AppendLine(line);
+                }
+            }
+
+            return sb;
+        }
+
+
+        private void readDialogButon_Click(object sender, RoutedEventArgs e)
+        {
+            Dictionary<string , string> contacts = new Dictionary<string , string>();
+            resultTextBox.Text = contacts.ToString();
+        }
+
+        private void readDictionaryButon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void writeButon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addButon_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
